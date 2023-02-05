79394537e71f2983cceb99ca5cdfcc40d860696e
KirillSSS <savchuk.kirill.ua@gmail.com>
2023-02-05 19:17:41 +0200

    Interface v2

--- Mushrooms_roots/Mushrooms_roots/Assets/Scripts/GameInterface.cs
+++ Mushrooms_roots/Mushrooms_roots/Assets/Scripts/GameInterface.cs
@@ -1,16 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class GameInterface : MonoBehaviour
 {
+    public GameObject[] foodIcons;
+    public GameObject[] noFoodIcons;
+    public GameObject[] waterIcons;
+    public GameObject[] noWaterIcons;
+    public GameObject[] mushroomsIcons;
+    public GameObject[] noMushroomsIcons;
+
+    public GameObject requiredEnergyIcon;
+    public GameObject extraMushroomsIcon;
+
+    public Text interactionField1;
+    public Text interactionField2;
+    public Text interactionField3;
+    public Text interactionField4;
+
     Text energyField;
     Text foodField;
     Text waterField;
     Text mushroomsField;
     Text speedField;
-    Text requiredResourcesField;
+    Text requiredEnergyField;
+    Text requiredMushroomsField;
 
     int energyAmount;
     int foodAmount;
@@ -19,9 +38,6 @@ public class GameInterface : MonoBehaviour
     int speedMultiplier;
     int requiredEnergyAmount;
     int requiredMushroomsNumber;
-    int extractedFoodAmount;
-    int extractedWaterAmount;
-    int extractedMushroomsNumber;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +47,8 @@ public class GameInterface : MonoBehaviour
         waterField = GameObject.Find("WaterField").GetComponent<Text>();
         mushroomsField = GameObject.Find("MushroomsField").GetComponent<Text>();
         speedField = GameObject.Find("SpeedField").GetComponent<Text>();
+        requiredEnergyField = GameObject.Find("RequiredEnergyField").GetComponent<Text>();
+        requiredMushroomsField = GameObject.Find("RequiredMushroomsField").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -41,8 +59,9 @@ public class GameInterface : MonoBehaviour
 
     public void Test()
     {
-        UpdateResourcesInfo(1, 2, 3, 4);
-        UpdateSpeedInfo(4);
+        UpdateExtractedResourcesInfo(Random.Range(0,6), Random.Range(0, 6), Random.Range(0, 6));
+
+        UpgradeTheZone(123, 345, 1);
     }
 
     public void UpdateResourcesInfo(int energy, int food, int water, int mushrooms)
@@ -57,6 +76,14 @@ public class GameInterface : MonoBehaviour
         UpdateResourcesFields();
     }
 
+    public void UpdateRequirementInfo(int energy, int mushrooms)
+    {
+        requiredEnergyAmount = energy;
+        requiredMushroomsNumber = mushrooms;
+
+        UpdateRequirementField();
+    }
+
     public void UpdateSpeedInfo(int speed)
     {
         speedMultiplier = speed;
@@ -64,6 +91,84 @@ public class GameInterface : MonoBehaviour
         Debug.Log(speedMultiplier + "");
 
         UpdateSpeedField();
+    }
+
+    public void UpdateExtractedResourcesInfo(int food, int water, int mushrooms)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            foodIcons[i].SetActive(false);
+            waterIcons[i].SetActive(false);
+            mushroomsIcons[i].SetActive(false);
+
+            noFoodIcons[i].SetActive(true);
+            noWaterIcons[i].SetActive(true);
+            noMushroomsIcons[i].SetActive(true);
+        }
+
+        for (int i = 0; i < food; i++)
+        {
+            foodIcons[i].SetActive(true);
+            noFoodIcons[i].SetActive(false);
+        }
+
+        for (int i = 0; i < water; i++)
+        {
+            waterIcons[i].SetActive(true);
+            noWaterIcons[i].SetActive(false);
+        }
+
+        for (int i = 0; i < mushrooms; i++)
+        {
+            mushroomsIcons[i].SetActive(true);
+            noMushroomsIcons[i].SetActive(false);
+        }
+    }
+
+    public void NoContactZone()
+    {
+        extraMushroomsIcon.SetActive(false);
+        requiredEnergyIcon.SetActive(false);
+
+        interactionField1.text = "You need";
+        interactionField2.text = "to open";
+        interactionField3.text = "nearby";
+        interactionField4.text = "zone";
+    }
+
+    public void GetTheZone(int energy, int seconds)
+    {
+        extraMushroomsIcon.SetActive(false);
+        requiredEnergyIcon.SetActive(true);
+
+        interactionField1.text = energy + "";
+        interactionField2.text = seconds + "sec";
+        interactionField3.text = "to open";
+        interactionField4.text = "the zone";
+    }
+
+    public void UpgradeTheZone(int energy, int seconds, int extraMushrooms)
+    {
+        extraMushroomsIcon.SetActive(true);
+        requiredEnergyIcon.SetActive(true);
+
+        interactionField1.text = energy + "";
+        interactionField2.text = seconds + "sec";
+        interactionField3.text = "to grow";
+        interactionField4.text = extraMushrooms + "";
+    }
+
+    public void MaxLimitOfTheZone()
+    {
+        extraMushroomsIcon.SetActive(false);
+        requiredEnergyIcon.SetActive(false);
+
+        Debug.Log(interactionField1 + " " + interactionField2 + " " + interactionField3 + " " + interactionField4);
+
+        interactionField1.text = "You have";
+        interactionField2.text = "reached";
+        interactionField3.text = "the max";
+        interactionField4.text = "level";
     }
 
     private void UpdateResourcesFields()
@@ -77,5 +182,11 @@ public class GameInterface : MonoBehaviour
     private void UpdateSpeedField()
     { 
         speedField.text = speedMultiplier + "x";
+    }
+
+    private void UpdateRequirementField()
+    { 
+        requiredEnergyField.text = requiredEnergyAmount + "x";
+        requiredMushroomsField.text= requiredMushroomsNumber + "x";
     }
 }
